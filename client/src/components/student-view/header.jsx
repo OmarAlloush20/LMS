0bff614016752c0f7c3f93d229942c6d16af5aa3
OmarAlloush <omarzabadani1@gmail.com>
2025-03-19 04:18:41 +0300

    fixed header

--- client/src/components/student-view/header.jsx
+++ client/src/components/student-view/header.jsx
@@ -1,8 +1,8 @@
 import { GraduationCap, TvMinimalPlay } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
-import { AuthContext } from "@/context/auth-context";
 import { useContext } from "react";
+import { AuthContext } from "@/context/auth-context";
 
 function StudentViewCommonHeader() {
   const navigate = useNavigate();
@@ -12,19 +12,24 @@ function StudentViewCommonHeader() {
     resetCredentials();
     sessionStorage.clear();
   }
+
   return (
     <header className="flex items-center justify-between p-4 border-b relative">
       <div className="flex items-center space-x-4">
         <Link to="/home" className="flex items-center hover:text-black">
           <GraduationCap className="h-8 w-8 mr-4 " />
-          <span className="font-extrabold md:text-xl text-[14px]">LMS</span>
+          <span className="font-extrabold md:text-xl text-[14px]">
+            LMS LEARN
+          </span>
         </Link>
         <div className="flex items-center space-x-1">
           <Button
             variant="ghost"
-            onClick={() =>
-              location.pathname("/courses") ? null : navigate("/courses")
-            }
+            onClick={() => {
+              location.pathname.includes("/courses")
+                ? null
+                : navigate("/courses");
+            }}
             className="text-[14px] md:text-[16px] font-medium"
           >
             Explore Courses
@@ -42,9 +47,7 @@ function StudentViewCommonHeader() {
             </span>
             <TvMinimalPlay className="w-8 h-8 cursor-pointer" />
           </div>
-          <div>
-            <Button onClick={handleLogout}>Sign Out</Button>
-          </div>
+          <Button onClick={handleLogout}>Sign Out</Button>
         </div>
       </div>
     </header>
